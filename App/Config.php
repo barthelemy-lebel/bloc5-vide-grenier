f0c12f34dbd27185c181d2aa317efd8126950d30
TerryGyselings <terry.gyselings@viacesi.fr>
2025-05-15 12:04:01 +0200

    docker-compose

--- App/Config.php
+++ App/Config.php
@@ -14,28 +14,25 @@ class Config
      * Database host
      * @var string
      */
-    const DB_HOST = 'localhost';
+    const DB_HOST = 'db';
 
     /**
      * Database name
      * @var string
      */
-    //const DB_NAME = 'videgrenierenligne';
-    const DB_NAME = 'videgrenier';
+    const DB_NAME = 'videgrenierenligne';
 
     /**
      * Database user
      * @var string
      */
-    //const DB_USER = 'webapplication';
-    const DB_USER = 'root';
+    const DB_USER = 'webapplication';
 
     /**
      * Database password
      * @var string
      */
-    //const DB_PASSWORD = '653rag9T';
-    const DB_PASSWORD = 'root';
+    const DB_PASSWORD = '653rag9T';
 
     /**
      * Show or hide error messages on screen
